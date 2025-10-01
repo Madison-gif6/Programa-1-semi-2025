14a36ce36387faf370e3602665f69f5804cbfa33
Madison-gif6 <madisonvj5@gmail.com>
2025-10-01 11:44:23 -0600

    Hola mundo, Programa Saludar

---
+++ MiPrimerProyectoenCshar/Saludar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiPrimerProyectoenCshar
+{
+    public partial class Saludar : Form
+    {
+        public Saludar()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaludar_Click(object sender, EventArgs e)
+        {
+            lblHola.Text = "¡Hola, " + txtNombre.Text + "!";    
+        }
+    }
+}
